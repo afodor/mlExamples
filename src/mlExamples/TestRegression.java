9d0aad85cadb2590f326c702e985bc398802a4c2
Anthony Fodor <anthony.fodor@gmail.com>
2016-11-21 14:15:46 -0500

    clean up Y example a bit...

--- src/mlExamples/TestRegression.java
+++ src/mlExamples/TestRegression.java
@@ -5,13 +5,14 @@ public class TestRegression
 {
 	public static void main(String[] args) throws Exception
 	{
-		double[][] y = {{73 , 50, 128, 170,  87, 108, 135,  69, 148, 132} };
+		double[][] y = {{73} , {50}, {128}, {170},  {87}, {108}, {135},  {69}, 
+				{148}, {132} };
 		double[][] x = { 
 				{1,30},{1,20},{1,60},{1,80},{1,40},{1,50},{1,60},{1,30},{1,70},{1,60}};
 		
 		Matrix X = new Matrix(x);
 		Matrix XT = X.transpose();
-		Matrix PRODUCT=  XT.times(X).inverse().times(XT).times(new Matrix(y).transpose());
+		Matrix PRODUCT=  XT.times(X).inverse().times(XT).times(new Matrix(y));
 		
 		printMatrix(PRODUCT);
 	}
